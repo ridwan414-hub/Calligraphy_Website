294f00f39411f361a2bd59041bae3cd1ff255484
anonymous <133980811+ridwan414-hub@users.noreply.github.com>
2024-06-04 03:04:47 +0600

    Top banner added

--- client/src/components/Form/SearchInput.js
+++ client/src/components/Form/SearchInput.js
@@ -23,17 +23,22 @@ const SearchInput = () => {
     return (
         <div>
             <form className="flex search-form" role="search" onSubmit={handleSubmit}>
-                <input
+                {/* <input
                     className="border border-gray-300 rounded-md p-2 mr-2"
                     type="search"
                     placeholder="Search"
                     aria-label="Search"
                     value={values.keyword}
                     onChange={(e) => setValues({ ...values, keyword: e.target.value })}
-                />
-                <button className="bg-green-500 text-white px-4 py-2 rounded-md" type="submit">
+                /> */}
+            <label className="input input-bordered flex items-center gap-2">
+                <input type="text" className="grow" placeholder="Search" value={values.keyword}
+                    onChange={(e) => setValues({ ...values, keyword: e.target.value })} />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+            </label>
+                {/* <button className="bg-green-500 text-white px-4 py-2 rounded-md" type="submit">
                     Search
-                </button>
+                </button> */}
             </form>
         </div>
     );
--- client/src/components/Layouts/Layout.js
+++ client/src/components/Layouts/Layout.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Footer from './Footer';
 import { Helmet } from 'react-helmet';
 import { Toaster } from 'react-hot-toast';
+import TopBanner from '../TopBanner';
 
 // import Sidebar from '../Sidebar';
 
@@ -23,12 +24,13 @@ const Layout = ({
                 <title>{title}</title>
             </Helmet>
             <Toaster
-                position="top-right"
+                position="top-center"
                 reverseOrder={false}
             />
+            <TopBanner/>
             <Header />
             <main
-                className='mx-auto px-4 max-w-screen-lg flex flex-col min-h-screen bg-aliceblue'>
+                className='mx-auto px-4 my-4 max-w-screen-lg flex flex-col min-h-screen bg-blue-50'>
                 {children}
             </main>
             <Footer />
