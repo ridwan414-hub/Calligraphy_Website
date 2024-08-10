55252a9ad9a50677bfa53e75abe7030de6a54f5f
ridwan414-hub <asifmahmoud414@gmail.com>
2024-08-10 16:54:30 +0600

    removed landing page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,17 +1,13 @@
-import React, { useState } from "react";
 import Banner from "../components/Banner";
 import HomepageProductsFilteringSection from "../components/page-components/home-page/Homepage-ProductsFiltering -Section";
 import Layout from "../components/Layouts/Layout";
-import LandingPage from "./LandingPage";
 
 const HomePage = () => {
-    const [boolean, setBoolean] = useState(true)
     return (
-        boolean ? (<LandingPage setBoolean={setBoolean} />) :
-            (<Layout title={'ALl Products - Best offers '}>
-                <Banner />
-                <HomepageProductsFilteringSection />
-            </Layout >)
+        (<Layout title={'ALl Products - Best offers '}>
+            <Banner />
+            <HomepageProductsFilteringSection />
+        </Layout >)
     )
 };
 
